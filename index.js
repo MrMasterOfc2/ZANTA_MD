3532bd878ac0dde11b69b097f4c3a67d20ea464e
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-11-24 22:39:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const prefix = '.';
-const ownerNumber = ['94743404814'];
+const ownerNumber = ['94743404814', '0743404814'];
 const authDir = path.join(__dirname, '/auth_info_baileys/');
 const credsPath = path.join(authDir, 'creds.json');
 
